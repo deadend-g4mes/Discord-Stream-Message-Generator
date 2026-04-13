c6574c72ae961742e06c0a699c66d02b126abba0
deadend-g4mes <leighton.h.carpenter@gmail.com>
2026-04-12 19:08:32 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from schedule import ScheduleBuilder
+from pyscript import web, when
+from datetime import datetime
+
+sb = ScheduleBuilder()
+
+inc = 0
+
+@when("click", "#continue")
+def input(event):
+    global inc
+    if inc < 6:
+        time_input = web.page["time-input"].value
+        title_input = web.page["title-input"].value
+        sb.title = title_input
+        if title_input == "Offline":
+            sb.date = datetime.strptime(time_input, "%m-%d-%Y")
+        else:
+            sb.date = datetime.strptime(time_input, "%m-%d-%Y %H:%M")
+        sb.timestamp_gen()
+        inc += 1
+    elif inc == 6:
+        time_input = web.page["time-input"].value
+        title_input = web.page["title-input"].value
+        sb.title = title_input
+        if title_input == "Offline":
+            sb.date = datetime.strptime(time_input, "%m-%d-%Y")
+        else:
+            sb.date = datetime.strptime(time_input, "%m-%d-%Y %H:%M")
+        sb.timestamp_gen()
+        button = web.page["continue"]
+        button.innerText = "Print Final Schedule"
+        inc = 7
+    else:
+        output = web.page["output"]
+        output.innerText = sb.week_schedule
---
+++ schedule.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from time import sleep
+
+class ScheduleBuilder:
+
+    def __init__(self):
+        self.week_schedule = ""
+        self.date = ""
+        self.title = "Output: \n"
+
+    def timestamp_gen(self):
+        """
+        year: YYYY - 2026
+        month: M - 4, 12
+        day: D - 1, 30
+        hour: H - 1, 13
+        minute: m - 1, 60
+        stream: title - Offline, Yapping & Playing Game
+        """
+        if self.title == "Offline":
+            self.week_schedule = self.week_schedule + f"<t:{int(datetime(self.date.year, self.date.month, self.date.day).timestamp())}:D> - {self.title}" + "\n"
+        else:
+            self.week_schedule = self.week_schedule + f"<t:{int(datetime(self.date.year, self.date.month, self.date.day, self.date.hour, self.date.minute).timestamp())}:F> - <t:{int(datetime(self.date.year, self.date.month, self.date.day, self.date.hour, self.date.minute).timestamp())}:R> - {self.title}" + "\n"
+
+
+
